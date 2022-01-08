fc26e1741367f418307f348fbe6fff4073f80f67
abdofawzy841 <58747792+abdofawzy841@users.noreply.github.com>
2022-01-08 22:19:06 +0200

    Update user_validation.sql

--- DataBase/user_validation.sql
+++ DataBase/user_validation.sql
@@ -35,14 +35,21 @@ DELIMITER ;
 
 
 USE `bookstore`;
-DROP procedure IF EXISTS `signUp`;
+DROP function IF EXISTS `signUp`;
 DELIMITER $$
 USE `bookstore`$$
-CREATE PROCEDURE signUp (user_name varchar(45), password varchar(15),
+CREATE FUNCTION signUp (userName varchar(45), password varchar(15),
 					 last_name varchar(45), first_name varchar(45), email varchar(45), 
 					 phone varchar(12),shipping_address varchar(45))
+ RETURNS boolean
 BEGIN
-	INSERT INTO user values(user_name, password, last_name, first_name, email, phone,shipping_address,false);
+    IF userName in (select user_name from user) THEN
+    return false;
+    ELSE 
+    INSERT INTO user values(user_name, password, last_name, first_name, email, phone,shipping_address,false);
+			RETURN true;
+	END IF;
+	
 END$$
 DELIMITER ;
 
@@ -53,6 +60,19 @@ USE `bookstore`$$
 CREATE PROCEDURE logout (userName varchar(45))
 BEGIN
 		DELETE FROM cart 
+		WHERE user_name = userName;
+END$$
+DELIMITER ;
+
+
+USE `bookstore`;
+DROP procedure IF EXISTS `getUser`;
+DELIMITER $$
+USE `bookstore`$$
+CREATE PROCEDURE getUser (userName varchar(45))
+BEGIN
+		SELECT*
+        FROM user 
 		WHERE user_name = userName;
 END$$
 DELIMITER ;
